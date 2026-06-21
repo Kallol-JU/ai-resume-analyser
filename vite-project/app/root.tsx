d819ae20a52a81fc27339511d85896f97290e1d6
Kallol Mukherjee <mukherjeekallol42@gmail.com>
2026-06-22 03:41:35 +0530

    error hunting

--- vite-project/app/root.tsx
+++ vite-project/app/root.tsx
@@ -32,8 +32,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+
       <body>
+        <script src="https://js.puter.com/v2/" async></script>
+
         {children}
+
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -56,7 +60,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
       error.status === 404
         ? "The requested page could not be found."
         : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
+  } else if (import.meta.env.DEV && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
@@ -65,8 +69,9 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
     <main className="pt-16 p-4 container mx-auto">
       <h1>{message}</h1>
       <p>{details}</p>
+
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
--- vite-project/app/routes/home.tsx
+++ vite-project/app/routes/home.tsx
@@ -2,22 +2,38 @@ import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "../components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
-    { name: "description", content: "Smart Feedback For Your Dream Role" },
+    {
+      name: "description",
+      content: "Smart Feedback For Your Dream Role",
+    },
   ];
 }
 
 export default function Home() {
+  const auth = usePuterStore((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated, navigate]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
         <div className="page-heading py-16">
-          <h1>Track Your Applications & Resume Ratings </h1>
-          <h2>Review Your Submissions And Get AI-Powered Feedback </h2>
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review Your Submissions And Get AI-Powered Feedback</h2>
         </div>
 
         {resumes.length > 0 && (
